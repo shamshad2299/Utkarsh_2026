9e7e33334937226f10b8283d41b1a2c4ab12196e
Threcia-01 <deepchaudhary29924@gmail.com>
2026-01-30 18:31:09 +0530

    Initialize backend with project structure, database connection, models, and environment setup

---
+++ backend/src/db/connectDB.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      process.env.MONGO_URI
+    );
+
+    console.log(
+      `MongoDB connected! DB host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.error("Mongoose Connection Error:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
