e6368988ee1bc6c782beff16bd10b5c8305c2b1e
shamashtech <shamashtech@gmail.com>
2026-02-09 04:10:35 +0530

    backend Url added

--- Frontend/src/admin/api/axios.js
+++ Frontend/src/admin/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:7000/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 let isRefreshing = false;
 let failedQueue = [];
