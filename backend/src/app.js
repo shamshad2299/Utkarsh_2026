23b98dd4bf8ec4c94f15d32548eb4cd973154260
shamashtech <shamashtech@gmail.com>
2026-02-09 04:35:39 +0530

    cors setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,13 +23,9 @@ app.use(cors({
   origin: process.env.CORS_ORIGIN,
   credentials: true,
   methods: ["GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization"
-  ]
+  allowedHeaders: [ "Content-Type","Authorization" ]
 }));
 
-app.options("*", cors());
 
 
 app.use(express.json());
